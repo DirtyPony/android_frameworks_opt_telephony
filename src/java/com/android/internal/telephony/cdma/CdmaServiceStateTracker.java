3cde9eb616b3267d1da4148d6aa66e1f152d5909
Bret Zamzow <mazwoz@gmail.com>
2013-10-06 20:32:09 -0700

    Revert "Add onDataRatChanged trigger to DcTracker"
    This reverts commit ae3217c9a32ab7f7dd4c0aecd9ca5f6e3022d9c3.

--- src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -1023,7 +1023,6 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
         if (hasRilDataRadioTechnologyChanged) {
             mPhone.setSystemProperty(TelephonyProperties.PROPERTY_DATA_NETWORK_TYPE,
                     ServiceState.rilRadioTechnologyToString(mSS.getRilDataRadioTechnology()));
-            mDataRatChangedRegistrants.notifyRegistrants();
         }
 
         if (hasRegistered) {
